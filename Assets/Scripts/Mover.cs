59fe929d6b8c717351744ab59ced15c7a0a7ea55
sergkom1 <serg5551@bk.ru>
2025-02-22 22:59:47 +0400

    Update1
    Player+Border

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,17 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+
 public class Mover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const float SPEED_CONF = 150;
+    private const string HORIZONTAL_AXIS = "Horizontal";
+    private const string VERTICAL_AXIS = "Vertical";
+    private const string BORDER_TAG = "Border";
+
+    [SerializeField] private float _speedX = 1; //движение по X
+    [SerializeField] private float _speedY = 1; //движение по Y
+    [SerializeField] private float _boostForce = 1; //Длинна буста
+
+    public Rigidbody2D _rigidbody;
+    private float _direction;
+    private bool _isBoost;
+    private bool _isGround;
+
+    private void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _direction = Input.GetAxis(HORIZONTAL_AXIS);
+
+        if (_isGround && Input.GetKeyDown(KeyCode.W))
+        {
+            _isBoost = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = new Vector2(_speedX * _direction * SPEED_CONF * Time.fixedDeltaTime, _rigidbody.velocity.y);
+        if (_isBoost)
+        {
+            _rigidbody.AddForce(new Vector2(0, _boostForce));
+            _isBoost = false;
+            _isGround = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(BORDER_TAG))
+            _isGround = true;
     }
 }
